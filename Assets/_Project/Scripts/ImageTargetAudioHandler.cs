55f709413b96d07a6ca6113d447f7b28577a2e9c
Ivan Gadzhega <ivgadzhega@gmail.com>
2016-07-20 15:10:35 +0300

    Play sound when target detected

---
+++ Assets/_Project/Scripts/ImageTargetAudioHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ImageTargetAudioHandler : MonoBehaviour, ITrackableEventHandler
+{
+    [SerializeField]
+    private AudioSource _audio;
+    private TrackableBehaviour _trackableBehaviour;
+
+    public void Awake()
+    {
+        _trackableBehaviour = GetComponent<TrackableBehaviour>();
+    }
+
+    protected void Start()
+    {
+        if (_trackableBehaviour) _trackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            _audio.Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            _audio.Stop();
+        }
+    }
+}
